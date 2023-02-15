9a83e2588ba62aa3e7213a3af65dbb4126e73b0b
quanle <leminhquan8101998@gmail.com>
2023-02-15 11:09:39 +0700

    add semantic ui and favicon

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,4 +4,6 @@ pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin "jquery", to: "https://code.jquery.com/jquery-3.1.1.min.js"
+pin "semantic-ui", to: "https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
